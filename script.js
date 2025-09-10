915b81f4b74649f05dd88d09686bd9ae8ebdfcab
ItsAxY420 <maximbondar666@gmail.com>
2025-09-10 21:52:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const API_KEY = "AIzaSyDNA7a_EgNLjO8DMGL6cy8RtUdASlQCGSk";
 const FOLDERS = {
   s1: "12QHYD56V3f7wGd04yChF1prfpwsb91Hc", // Season 1
   s2: "1cdeSnoOsSEXYAlOQfCGqN97VApK2PmJ6",
-  s3: "1_9q3Ay6qADCNdSqBCDJ5cg0ZQ7FaTm4N",
+  s3: "1VzrJLiby1QznKp_vBcBECtCQvZYpHWyH",
   s4: "1Kck7onbfUX6l8xwwsK2eXe0D6YwaYJSf",
   s5: "11z6r_KKDqasqkuNDjF2_gavDqkuT1BOh",
   s6: "1MfL6TzzV8k7n6cEqYXh62vfge2pqYjjV",
